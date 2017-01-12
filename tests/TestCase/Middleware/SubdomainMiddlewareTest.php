8e64a635b0d1346352abc877624ec5f2d8a6c3d7
AJ Quick <ajquick@gmail.com>
2017-01-11 21:38:44 -0800

    Update SubdomainMiddlewareTest

--- tests/TestCase/Middleware/SubdomainMiddlewareTest.php
+++ tests/TestCase/Middleware/SubdomainMiddlewareTest.php
@@ -22,41 +22,42 @@ use Cake\Core\Configure;
 
 class SubdomainMiddlewareTest extends TestCase {
   
-  private $subdomainMiddleware;
-  
-  public function setUp() {
-      parent::setUp();
-      Configure::write('Multidimensional/Subdomains.Subdomains', ['admin']);
-      $this->SubdomainMiddleware = new SubdomainMiddleware();
-  }
-  
-  public function tearDown() {
-      parent::tearDown();
-      Configure::delete('Multidimensional/Subdomains.Subdomains');
-      unset($this->SubdomainMiddleware);
-  }
-  
-  public function testGetSubdomains() {
-      $subdomains = $this->SubdomainMiddleware->getSubdomains();
-      $this->assertEquals($subdomains, ['admin']);
-  }
-  
-  public function testGetPrefixAndHost() {
-      $array = $this->SubdomainMiddleware->getPrefixAndHost('admin.example.com');
-      $this->assertEquals($array, ['admin', 'example.com']);
-      unset($array);
-      $array = $this->SubdomainMiddleware->getPrefixAndHost('subdomain.example.com');
-      $this->assertEquals($array, [false, 'example.com']);
-      unset($array);
-      $array = $this->SubdomainMiddleware->getPrefixAndHost('example.com');
-      $this->assertEquals($array, [false, 'example.com']);
-  }
-  
-  public function testInvoke() {
-      //$this->configRequest(['uri' => ['_host' => 'admin.example.com']]);
-      $request = $this->getMockBuilder('Psr\Http\Message\ServerRequestInterface')->getMock();
-      $response = $this->getMockBuilder('Psr\Http\Message\ResponseInterface')->getMock();
-      //$this->SubdomainMiddleware->__invoke($request, $resposne, $name);
-  }
-  
+    private $subdomainMiddleware;
+
+    public function setUp() {
+        parent::setUp();
+        Configure::write('Multidimensional/Subdomains.Subdomains', ['admin']);
+        $this->SubdomainMiddleware = new SubdomainMiddleware();
+        $this->useHttpServer(true);
+    }
+
+    public function tearDown() {
+        parent::tearDown();
+        Configure::delete('Multidimensional/Subdomains.Subdomains');
+        unset($this->SubdomainMiddleware);
+    }
+
+    public function testGetSubdomains() {
+        $subdomains = $this->SubdomainMiddleware->getSubdomains();
+        $this->assertEquals($subdomains, ['admin']);
+    }
+
+    public function testGetPrefixAndHost() {
+        $array = $this->SubdomainMiddleware->getPrefixAndHost('admin.example.com');
+        $this->assertEquals($array, ['admin', 'example.com']);
+        unset($array);
+        $array = $this->SubdomainMiddleware->getPrefixAndHost('subdomain.example.com');
+        $this->assertEquals($array, [false, 'example.com']);
+        unset($array);
+        $array = $this->SubdomainMiddleware->getPrefixAndHost('example.com');
+        $this->assertEquals($array, [false, 'example.com']);
+    }
+
+    public function testInvoke() {
+        //$this->configRequest(['uri' => ['_host' => 'admin.example.com']]);
+        //$request = $this->getMockBuilder('Psr\Http\Message\ServerRequestInterface')->getMock();
+        //$response = $this->getMockBuilder('Psr\Http\Message\ResponseInterface')->getMock();
+        //$this->SubdomainMiddleware->__invoke($request, $resposne, $name);
+    }
+
 }
