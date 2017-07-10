9710046bde46602f80eb33854748fb5821cfc591
AJ Quick <ajquick@gmail.com>
2017-07-10 17:11:17 -0600

    Update Copyrights. Expand Travis CI to include more versions of CakePHP for testing. Expand Composer.json to allow more versions of CakePHP and specific versions of PHPUnit.

--- tests/TestCase/Middleware/SubdomainMiddlewareTest.php
+++ tests/TestCase/Middleware/SubdomainMiddlewareTest.php
@@ -1,5 +1,11 @@
 <?php
 /**
+ *      __  ___      ____  _     ___                           _                    __
+ *     /  |/  /_  __/ / /_(_)___/ (_)___ ___  ___  ____  _____(_)___  ____   ____ _/ /
+ *    / /|_/ / / / / / __/ / __  / / __ `__ \/ _ \/ __ \/ ___/ / __ \/ __ \ / __ `/ /
+ *   / /  / / /_/ / / /_/ / /_/ / / / / / / /  __/ / / (__  ) / /_/ / / / // /_/ / /
+ *  /_/  /_/\__,_/_/\__/_/\__,_/_/_/ /_/ /_/\___/_/ /_/____/_/\____/_/ /_(_)__,_/_/
+ *
  * CakePHP Plugin : CakePHP Subdomain Routing
  * Copyright (c) Multidimension.al (http://multidimension.al)
  * Github : https://github.com/multidimension-al/cakephp-subdomains
@@ -8,9 +14,9 @@
  * For full copyright and license information, please see the LICENSE file
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright (c) Multidimension.al (http://multidimension.al)
- * @link      https://github.com/multidimension-al/cakephp-subdomains Github
- * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ * @copyright  Copyright © 2016-2017 Multidimension.al (http://multidimension.al)
+ * @link       https://github.com/multidimension-al/cakephp-subdomains Github
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
 namespace Multidimensional\Subdomains\Tests\TestCase\Middleware;
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,11 @@
 <?php
 /**
+ *      __  ___      ____  _     ___                           _                    __
+ *     /  |/  /_  __/ / /_(_)___/ (_)___ ___  ___  ____  _____(_)___  ____   ____ _/ /
+ *    / /|_/ / / / / / __/ / __  / / __ `__ \/ _ \/ __ \/ ___/ / __ \/ __ \ / __ `/ /
+ *   / /  / / /_/ / / /_/ / /_/ / / / / / / /  __/ / / (__  ) / /_/ / / / // /_/ / /
+ *  /_/  /_/\__,_/_/\__/_/\__,_/_/_/ /_/ /_/\___/_/ /_/____/_/\____/_/ /_(_)__,_/_/
+ *
  * CakePHP Plugin : CakePHP Subdomain Routing
  * Copyright (c) Multidimension.al (http://multidimension.al)
  * Github : https://github.com/multidimension-al/cakephp-subdomains
@@ -8,18 +14,15 @@
  * For full copyright and license information, please see the LICENSE file
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     (c) Multidimension.al (http://multidimension.al)
- * @link          https://github.com/multidimension-al/cakephp-subdomains Github
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ * @copyright  Copyright © 2016-2017 Multidimension.al (http://multidimension.al)
+ * @link       https://github.com/multidimension-al/cakephp-subdomains Github
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
 // @codingStandardsIgnoreFile
 
-use Cake\Cache\Cache;
 use Cake\Core\Configure;
 use Cake\Core\Plugin;
-use Cake\Datasource\ConnectionManager;
-use Cake\I18n\I18n;
 
 require_once 'vendor/autoload.php';
 
